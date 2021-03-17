a5a15e587eaa986f561abbbd957624de1f28b07a
tgowda1996 <71807910+tgowda1996@users.noreply.github.com>
2021-03-17 15:48:48 -0500

    Test cases for lock, spinLock, condVar public methods (#13)

--- project_2/tests_public_methods.cpp
+++ project_2/tests_public_methods.cpp
@@ -14,35 +14,39 @@ void check_conVar_broadcast_and_hoare_semantics();
 void check_lock_unlock();
 void check_spin_lock_unlock();
 
-using std::cout
+using std::cout;
+using std::endl;
 
 void * worker_wait_for_signal(void* args) {
     int my_tid = uthread_self();
     lock.lock();
-    cout<<"Thread " << my_tid << " waiting";
+    cout<<"Thread " << my_tid << " waiting\n";
     condVar.wait(lock);
     cout<<"got a signal. Thread "<<my_tid<<" starts working\n";
     lock.unlock();
+    return NULL;
 }
 
 void * signalOtherThread(void* args) {
     int my_tid = uthread_self();
     lock.lock();
     cout<<"Starting thread " << my_tid << "\n";
-    cout<<"Thread "<< my_tid <<" working\n"
+    cout<<"Thread "<< my_tid <<" working\n";
     condVar.signal();
     cout<<"Thread "<<my_tid<<" work after signal\n";
     lock.unlock();
+    return NULL;
 }
 
 void * broadcastSignal(void* args) {
     int my_tid = uthread_self();
     lock.lock();
     cout<<"Starting thread " << my_tid << "\n";
-    cout<<"Thread "<< my_tid <<" working\n"
+    cout<<"Thread "<< my_tid <<" working\n";
     condVar.broadcast();
     cout<<"Thread "<<my_tid<<" work after signal\n";
     lock.unlock();
+    return NULL;
 }
 
 void * work_thread(void* args) {
@@ -50,34 +54,54 @@ void * work_thread(void* args) {
     lock.lock();
     cout<<"Thread "<<my_tid<<" working\n";
     lock.unlock();
+    return NULL;
 }
 
 void * work_thread_lock_testing(void* args) {
     int my_tid = uthread_self();
+    cout<<"Starting thread "<<my_tid<<endl;
     lock.lock();
     cout<<"Thread "<<my_tid<<" starts working\n";
     for (int i = 0; i < 1000; i++)
-        for (int j = 0; j < 1000; i++)
-            for (int k = 0; k < 1000; i++);
+        for (int j = 0; j < 100; j++)
+            for (int k = 0; k < 100; k++);
     cout<<"Thread "<<my_tid<<" finished working\n";
     lock.unlock();
+    return NULL;
 }
 
 void * work_thread_spin_lock_testing(void* args) {
     int my_tid = uthread_self();
+    cout<<"Starting thread "<<my_tid<<endl;
     spinLock.lock();
     cout<<"Thread "<<my_tid<<" starts working\n";
     for (int i = 0; i < 1000; i++)
-        for (int j = 0; j < 1000; i++)
-            for (int k = 0; k < 1000; i++);
+        for (int j = 0; j < 100; j++)
+            for (int k = 0; k < 100; k++);
     cout<<"Thread "<<my_tid<<" finished working\n";
     spinLock.unlock();
+    return NULL;
 }
 
-int main(){
+int main(int argc, char* argv[]){
     uthread_init(100);
-    check_conVar_and_hoare_sematics();
 
+    if (argc < 2) {
+	    std::cerr << "Usage: ./test_public_methods <test_case_number>" << endl;
+	    exit(1);
+    }
+    int tn = atoi(argv[1]);
+    if (tn == 1) check_conVar_and_hoare_semantics();    
+    else if (tn == 2) check_conVar_broadcast_and_hoare_semantics();
+    else if (tn == 3) check_lock_unlock();
+    else if (tn == 4) check_spin_lock_unlock();    
+    else cout <<"Wrong test case number\n";
+    return 0;
+
+    //check_conVar_and_hoare_semantics();
+    //check_conVar_broadcast_and_hoare_semantics();
+    //check_lock_unlock();
+    //check_spin_lock_unlock();
 
     cout<<"Exiting"<<endl;
 }
