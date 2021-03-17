cea22e210113496e985a17800ca4a8b744af8f48
Tushar Gowda <gowda019@umn.edu>
2021-03-17 16:39:55 -0500

    async test correction

--- project_2/async_test.cpp
+++ project_2/async_test.cpp
@@ -16,10 +16,6 @@ using std::endl;
 #define SECOND_LOOP 1000
 #define THIRD_LOOP 1000
 #define FILE_NAME "to_read"
-#define READ_WRITE_OPERATIONS 1
-#define TYPE SYNC
-#define SYNC 1
-#define ASYNC 2
 
 struct loop_args{
     int i, j, k;
@@ -55,10 +51,7 @@ void * worker_async_calls(void* args) {
     int bytes_read = 1, offset = 0;
     int fd = open(file_name, O_RDONLY);
     int wfd = open("to_write", O_WRONLY|O_CREAT);
-    //while(bytes_read){
     bytes_read = async_read(fd, buffer, BUFFER_SIZE, offset);
-        //offset += bytes_read;
-    //}
     async_write(wfd, buffer, bytes_read, 0);
     cout<<"Done with read and write\n";
     delete buffer;
@@ -67,7 +60,6 @@ void * worker_async_calls(void* args) {
 
 void * worker_async_write(void* args) {
     int my_tid = uthread_self();
-    //const char * file_name = "spam.csv";
     const char * file_name = "to_write";
     cout<<"Starting thread : "<<my_tid<<endl;
     char buffer[] = "test";
@@ -75,19 +67,27 @@ void * worker_async_write(void* args) {
     int fd = open(file_name, O_WRONLY|O_CREAT);
     async_write(fd, buffer, 5, offset);
     cout<<"Done with write\n";
-    //delete buffer;
     return NULL;
 }
 
 int main(){
     uthread_init(10000);
+    if (argc < 2) {
+        std::cerr << "Usage: ./test_async_methods <test_case_number>" << endl;
+        exit(1);
+    }
+    int tn = atoi(argv[1]);
+    if (tn > 2) {
+        std::cerr << "Test case number can be either 1 or 2" << endl;
+    }
+
     int threads[2];
     //const char* file_name = "to_read";
     loop_args* largs = getLoopArgs(FIRST_LOOP, SECOND_LOOP, THIRD_LOOP);
     io_args* ioargs = getIOArgs(FILE_NAME);
     threads[0] = uthread_create(worker_simulation_long_calculation, (void *)largs);
-    //threads[1] = uthread_create(worker_async_calls, (void *)ioargs);
-    threads[1] = uthread_create(worker_async_write, (void *)ioargs);
+    if (tn == 1) threads[1] = uthread_create(worker_async_calls, (void *)ioargs);
+    else threads[1] = uthread_create(worker_async_write, (void *)ioargs);
 
     cout<<"Threads Created\n";
     for (int i = 0; i < 2; i++) {
